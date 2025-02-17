3d5a1d3f6365d8ce6e138426fd2710edbe44fae5
cryptrunner49 <cryptrunner49@proton.me>
2025-02-17 22:54:25 +0000

    Update enums

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -2,5 +2,5 @@ class Interview < ApplicationRecord
   belongs_to :job_application
 
   enum :mode, [ :in_person, :video, :phone ], prefix: true
-  enum :interview_type, [ :technical, :hr, :final ], prefix: true # Adjust enum values as needed.
+  enum :interview_type, [ :technical, :hr, :final, :other ], prefix: true # Adjust enum values as needed.
 end
