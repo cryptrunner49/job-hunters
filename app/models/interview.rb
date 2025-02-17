6d4c7b14f8d71344150cbf2ffad192db644f2363
cryptrunner49 <cryptrunner49@proton.me>
2025-02-17 22:40:29 +0000

    Fix enums

--- app/models/interview.rb
+++ app/models/interview.rb
@@ -1,6 +1,6 @@
 class Interview < ApplicationRecord
   belongs_to :job_application
 
-  enum mode: { none: 0, in_person: 1, video: 2, phone: 3 }
-  enum interview_type: { none: 0, technical: 1, hr: 2, final: 3 }  # Adjust enum values as needed.
+  enum :mode, [ :in_person, :video, :phone ], prefix: true
+  enum :interview_type, [ :technical, :hr, :final ], prefix: true # Adjust enum values as needed.
 end
