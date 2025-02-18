8138c8ef069b8066f4138a5afa22ae187bbcc26b
cryptrunner49 <cryptrunner49@proton.me>
2025-02-18 16:27:19 +0000

    Update Interviews controller

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,9 +1,10 @@
 class InterviewsController < ApplicationController
   before_action :set_interview, only: %i[ show edit update destroy ]
+  before_action :set_job_application, only: %i[ index new create ]
 
   # GET /interviews or /interviews.json
   def index
-    @interviews = Interview.all
+    @interviews = @job_application.interviews
   end
 
   # GET /interviews/1 or /interviews/1.json
@@ -12,20 +13,20 @@ class InterviewsController < ApplicationController
 
   # GET /interviews/new
   def new
-    @interview = Interview.new
+    @interview = @job_application.interviews.build
   end
 
   # GET /interviews/1/edit
   def edit
   end
 
-  # POST /interviews or /interviews.json
+  # POST /interviews or /interviewaas.json
   def create
-    @interview = Interview.new(interview_params)
+    @interview = @job_application.interviews.build(interview_params)
 
     respond_to do |format|
       if @interview.save
-        format.html { redirect_to @interview, notice: "Interview was successfully created." }
+        format.html { redirect_to job_application_interview_path(@job_application, @interview), notice: "Interview was successfully created." }
         format.json { render :show, status: :created, location: @interview }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,7 @@ class InterviewsController < ApplicationController
   def update
     respond_to do |format|
       if @interview.update(interview_params)
-        format.html { redirect_to @interview, notice: "Interview was successfully updated." }
+        format.html { redirect_to job_application_interview_path(@interview.job_application, @interview), notice: "Interview was successfully updated." }
         format.json { render :show, status: :ok, location: @interview }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +53,7 @@ class InterviewsController < ApplicationController
     @interview.soft_delete
 
     respond_to do |format|
-      format.html { redirect_to interviews_path, status: :see_other, notice: "Interview was successfully destroyed." }
+      format.html { redirect_to job_application_interviews_path(@interview.job_application), status: :see_other, notice: "Interview was successfully deleted." }
       format.json { head :no_content }
     end
   end
@@ -60,11 +61,15 @@ class InterviewsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_interview
-      @interview = Interview.find(params.expect(:id))
+      @interview = Interview.find(params[:id])
+    end
+
+    def set_job_application
+      @job_application = JobApplication.find(params[:job_application_id])
     end
 
     # Only allow a list of trusted parameters through.
     def interview_params
-      params.expect(interview: [ :job_application_id, :scheduled_date, :result, :mode, :interview_type, :notes ])
+      params.require(:interview).permit(:job_application_id, :scheduled_date, :result, :mode, :interview_type, :notes)
     end
 end
