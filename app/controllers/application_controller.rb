f9a4b42ec2c490a5b84c17ddc1fff453ea92240d
cryptrunner49 <cryptrunner49@proton.me>
2025-02-18 09:33:01 +0000

    Update Home Page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_hunter, :hunter_signed_in?
+
+  def current_hunter
+    @current_hunter ||= Hunter.find(session[:hunter_id]) if session[:hunter_id]
+  end
+
+  def hunter_signed_in?
+    current_hunter.present?
+  end
 end
